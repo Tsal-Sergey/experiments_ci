a0c63fdc4505d5790cebdd1856d7a96120dc7c76
System Administrator <root@val.local>
2017-01-19 04:09:18 +1200

    rp enchanced

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ server.use(restify.bodyParser());
 server.use(restify.queryParser());
 
 server.get("test",function (req, res, next) {
-    res.json({"prop":"val"});
+    res.json({"prop":"val5"});
     next();
 });
 server.listen(8766, function(){
-    process.send({ foo: 'bar4' });
+    process.send({ foo: 'bar5' });
 });
 
 
