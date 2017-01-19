97c0d04c73efb802a550e04462df8c5d94552030
Segrey Tsal <tsal.sergey.z@gmail.com>
2017-01-19 23:58:45 +1200

    val 32

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ server.use(restify.bodyParser());
 server.use(restify.queryParser());
 
 server.get("test",function (req, res, next) {
-    res.json({"prop":"val30"});
+    res.json({"prop":"val32"});
     next();
     next();
 });
 server.listen(8766, function(){
-    process.send({ foo: 'bar30' });
+    process.send({ foo: 'bar32' });
 });
 
 
