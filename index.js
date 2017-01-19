2f1b9da0110f00eae70e4091cc9866f4c26cb6ed
Segrey Tsal <tsal.sergey.z@gmail.com>
2017-01-19 23:50:50 +1200

    val 29

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ server.use(restify.bodyParser());
 server.use(restify.queryParser());
 
 server.get("test",function (req, res, next) {
-    res.json({"prop":"val28"});
+    res.json({"prop":"val29"});
     next();
     next();
 });
