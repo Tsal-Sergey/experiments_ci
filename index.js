1b20e496b79948669fac0036867eec704874ceb9
Segrey Tsal <tsal.sergey.z@gmail.com>
2017-01-21 01:32:28 +1200

    val 002

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ server.use(restify.bodyParser());
 server.use(restify.queryParser());
 
 server.get("test",function (req, res, next) {
-    res.json({"prop":"val001"});
+    res.json({"prop":"val002"});
     next();
 });
 server.listen(8766, function(){
-    process.send({ foo: 'bar001' });
+    process.send({ foo: 'bar002' });
 });
 
 
