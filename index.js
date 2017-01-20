3570d194cd13356af25d5c5c2ca164e4ac34b54c
Segrey Tsal <tsal.sergey.z@gmail.com>
2017-01-21 01:42:09 +1200

    val 006

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ server.use(restify.bodyParser());
 server.use(restify.queryParser());
 
 server.get("test",function (req, res, next) {
-    res.json({"prop":"val005"});
+    res.json({"prop":"val006"});
     next();
 });
 server.listen(8766, function(){
-    process.send({ foo: 'bar005' });
+    process.send({ foo: 'bar006' });
 });
 
 
