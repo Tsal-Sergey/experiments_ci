14cb00c5407cc2aa7f976cc7b083aa2d42120db5
Segrey Tsal <tsal.sergey.z@gmail.com>
2017-01-25 02:57:47 +1200

    success 2

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ server.use(restify.bodyParser());
 server.use(restify.queryParser());
 
 server.get("test1",function (req, res, next) {
-    res.json({"prop":"test2"});
+    res.json({"prop":"test1"});
     next();
 });
 server.listen(8766, function(){
-    process.send &&process.send({ foo: 'success 1' });
+    process.send &&process.send({ foo: 'success 2' });
 });
 
 
