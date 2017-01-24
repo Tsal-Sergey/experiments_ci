f49c68cb2a090d871476b419417c925ee3faf562
Segrey Tsal <tsal.sergey.z@gmail.com>
2017-01-25 02:02:00 +1200

    my commit a2

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ server.get("test2",function (req, res, next) {
     next();
 });
 server.listen(8766, function(){
-    process.send && process.send({ foo: 'bar00a1' });
+    process.send && process.send({ foo: 'bar00a2' });
 });
 
 
