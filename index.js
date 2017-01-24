ca4f45f812a9f8a7deaf2c4e695e59aa7d631ddc
Segrey Tsal <tsal.sergey.z@gmail.com>
2017-01-24 21:13:41 +1200

    my commit number 27

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ server.get("test",function (req, res, next) {
     next();
 });
 server.listen(8766, function(){
-    process.send({ foo: 'bar0026' });
+    process.send({ foo: 'bar0027' });
     //head_commit.url
 });
 
