8fd735b4218a8ec12fc5b67a2a2facc9e23def5b
Segrey Tsal <tsal.sergey.z@gmail.com>
2017-01-24 21:26:39 +1200

    my commit number 30

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ server.use(restify.bodyParser());
 server.use(restify.queryParser());
 
 server.get("test",function (req, res, next) {
-    res.json({"prop":"val0018"});
+    res.json({"prop":"val0029"});
     next();
 });
 server.listen(8766, function(){
-    process.send({ foo: 'bar0028' });
+    process.send({ foo: 'bar0030' });
     //head_commit.url
 });
 
